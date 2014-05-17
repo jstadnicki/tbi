1bc0433469ce34a7668fb0eb93e4e01f50b6882a
jstadnicki <jaroslaw.stadnicki@gmail.com>
2014-05-17 22:08:20 +0200

    native wpf application + unity as DIC

---
+++ Application/WpfNative/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace WpfNative
+{
+    using Microsoft.Practices.Unity;
+
+    using ToBeImplemented.Business.Implementations;
+    using ToBeImplemented.Business.Interfaces;
+    using ToBeImplemented.Business.Mapper;
+    using ToBeImplemented.Infrastructure.EFContext;
+    using ToBeImplemented.Infrastructure.Repository;
+    using ToBeImplemented.Service.Implementations;
+    using ToBeImplemented.Service.Interfaces;
+
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<ITbiContext, TbiContext>();
+            container.RegisterType<IConceptRepository, ConceptRepository>();
+            container.RegisterType<ITagRepository, TagRepository>();
+            container.RegisterType<IConceptService, ConceptService>();
+            container.RegisterType<IConceptLogic, ConceptLogic>();
+            container.RegisterType<MainWindow>();
+
+            TbiMapper.Initialize();
+
+
+            var mainwindow = container.Resolve<MainWindow>();
+            mainwindow.Show();
+
+
+        }
+    }
+}
---
+++ Application/WpfNative/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfNative
+{
+    using ToBeImplemented.Business.Interfaces;
+    using ToBeImplemented.Domain.ViewModel;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IConceptLogic conceptLogic;
+
+        public MainWindow(IConceptLogic conceptLogic)
+        {
+            this.conceptLogic = conceptLogic;
+            InitializeComponent();
+        }
+
+        private void ConceptOnClick(object sender, RoutedEventArgs e)
+        {
+            var result = this.conceptLogic.List();
+            this.conceptList.ItemsSource = result.Concepts;
+        }
+
+        private void UpdateSelected(object sender, RoutedEventArgs e)
+        {
+            var selected = conceptList.SelectedItem as ConceptViewModel;
+            var updateConceptViewModel = new UpdateConceptViewModel
+            {
+                AuthorId = selected.AuthorId,
+                Description = selected.Description,
+                Id = selected.Id,
+                Title = selected.Title
+            };
+            this.conceptLogic.UpdateConcept(updateConceptViewModel);
+        }
+    }
+}
