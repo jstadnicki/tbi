ba2c3eb02da2ab694d84d93a49b9ff200ea615f6
jstadnicki <jaroslaw.stadnicki@gmail.com>
2014-05-25 12:41:28 +0200

    adding new concept from SPA

---
+++ Application/Angular/application/controllers/AddConceptController.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="c:\source\ToBeImplemented\Application\Angular\scripts\angular.js" />
+angular.module('ToBeImplemented')
+    .controller('AddConceptController', function ($scope, $http, $location) {
+        $scope.concept = {};
+        $scope.init = function () {
+            $scope.concept.AuthorId = 1;
+        };
+
+        $scope.addConcept = function () {
+            var title = $scope.concept.Title;
+            var description = $scope.concept.Description;
+            var tags = $scope.concept.Tags;
+            var authorId = $scope.concept.AuthorId;
+            var id = $scope.concept.Id;
+
+            var concept = {
+                Id: id,
+                Title: title,
+                AuthorId: authorId,
+                Description: description,
+                Tags: tags
+            };
+
+            $http.post('http://localhost:7397/concepts', concept)
+                .success(function (data) {
+                    var id = angular.fromJson(data.Data);
+                    $location.path('/concepts/details/' + id);
+                })
+                .error(function (data, status) {
+                    $scope.error = data;
+                });
+        };
+
+    });
