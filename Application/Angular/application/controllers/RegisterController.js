ca1f01bf4fd07216c1be5816b2823b82c5abee59
jstadnicki <jaroslaw.stadnicki@gmail.com>
2014-07-06 23:27:18 +0200

    registration with busy indicator

--- Application/Angular/application/controllers/RegisterController.js
+++ Application/Angular/application/controllers/RegisterController.js
@@ -13,32 +13,39 @@ angular.module('ToBeImplemented')
         };
 
         $scope.registrationErrors = [];
+        $scope.isBusy = {
+            isLoading: false,
+            isSaving: false
+        };
 
         $scope.init = function () {
+            $scope.isBusy.isLoading = true;
+            $scope.isBusy.isSaving = false;
             $http.get(serviceAddress + '/register')
                 .success(function (response) {
                     $scope.registrationViewModel = angular.fromJson(response.Data);
+                    $scope.isBusy.isLoading = false;
                 })
                 .error(function (response) {
                     alert(response);
+                    $scope.isBusy.isLoading = false;
                 });
         };
 
         $scope.register = function () {
+            $scope.isBusy.isSaving = true;
             var result = authorizationService.register($scope.registrationViewModel);
             result
                 .success(function (opearationResult) {
+                    $scope.isBusy.isSaving = false;
                     if (opearationResult.Success) {
                         $location.path('/login');
                     } else {
                         $scope.registrationErrors = opearationResult.Errors;
                     }
                 })
-                .error(function () {});
+                .error(function () {
+                    $scope.isBusy.isSaving = false;
+                });
         };
-
-        $scope.passwordsMatches = function (a, b) {
-            console.log(a);
-            console.log(b);
-        }
     });
